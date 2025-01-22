9968b1d476734eccd9035f81a31a4b34c9c82627
heejae369 <heejae369@gmail.com>
2025-01-23 01:31:53 +0900

    fix: 리뷰페이지에는 use client 추가 나머지는 변경 없음

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
-
 const baseQuery = fetchBaseQuery({
   baseUrl,
   prepareHeaders: (headers) => {
